fb5308f60c96c0c5be8ff896cc6e581406a2d731
Suvayu Ali <fatkasuvayu+vc@gmail.com>
2010-07-08 11:34:38 -0700

    Major changes to org-mode and other text-mode like mode settings. Modularised many things including skeletons.

---
+++ org-mode-settings.el
@@ -0,0 +1,68 @@
+;;; org-mode-settings.el --- `org-mode' settings and customisations.
+
+;; since using org-mode in /opt/emacs-lisp
+(require 'org-install)
+
+;;; Code:
+
+;; autoload, useful to check if `org-mode' is loaded
+(autoload 'org-mode-p "org-macs"
+  "Check if the current buffer is in Org-mode." t)
+
+
+;; `org-mode' variable customisations
+;; directory to look for agenda files matching `org-agenda-file-regexp'
+(setq org-agenda-files '("~/org"))
+;; modifying behaviour of C-a/<home> & C-e/<end>
+(setq org-special-ctrl-a/e t)
+
+
+;; TODO keywords
+;; @ - time stamp with note
+;; ! - only time stamp
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "DLAY(l@)" "CONT(c!)" "|" "DONE(d!)" "CNCL(n@)")
+	(sequence "WInP(w)" "DBUG(b)" "|" "CMIT(m@)")
+	(type "PBUG(p@)" "CBUG(c@)" "SEGF(s@)" "|" "FIXD(f@)")))
+;; ;; TODO keyword faces
+;; (setq org-todo-keyword-faces
+;;       '(("" . ())
+;; 	))
+
+
+;; TAG faces
+(setq org-tag-faces
+      '(("PROJ" :background "firebrick3" :foreground "cornsilk2" :weight bold)))
+;; (setq org-tag-persistent-alist
+;;       '(("physics-bug" . 112)
+;; 	("code-bug" . 99)
+;; 	("segfault" . 115)
+;; 	("Project" . 80)))
+
+
+;; custom keymap
+(defun my-org-mode-keymap()
+  "My `org-mode' keymap."
+  (local-set-key (kbd "C-<left>") 'outline-up-heading)
+  ;; (local-set-key '[(C-right)] 'outline-back-to-heading) ; this one is not interactive
+  (local-set-key (kbd "C-<up>") 'outline-previous-visible-heading)
+  (local-set-key (kbd "C-<down>") 'outline-next-visible-heading)
+  (local-set-key (kbd "s-<up>") 'outline-backward-same-level)
+  (local-set-key (kbd "s-<down>") 'outline-forward-same-level))
+
+
+;; hooks
+;; `org-agenda-mode' hook
+(defun my-org-agenda-mode-hook()
+  "My `org-agenda-mode' hook."
+  (visual-line-mode t))
+(add-hook 'org-agenda-mode-hook 'my-org-agenda-mode-hook)
+
+;; `org-mode' hook
+(defun my-org-mode-hook()
+  "My `org-mode' hook."
+  ;; turn on auto-fill
+  (turn-on-auto-fill)
+  (my-org-mode-keymap))
+
+;;; org-mode-settings.el ends here
---
+++ skeletons.el
@@ -0,0 +1,76 @@
+;;; skeletons.el
+
+;;; skeleton definitions
+
+;; skeleton pair insert
+(setq skeleton-pair t)
+(global-set-key "(" 'skeleton-pair-insert-maybe)
+(global-set-key "{" 'skeleton-pair-insert-maybe)
+(global-set-key "[" 'skeleton-pair-insert-maybe)
+(global-set-key "\"" 'skeleton-pair-insert-maybe)
+
+;; (defvar skeletons-alist
+;;   '((?\( . ?\))
+;;     (?{  . ?})
+;;     (?[  . ?])
+;;     (?\" . ?\")))
+
+;; (defadvice delete-backward-char (before delete-empty-pair activate)
+;;   (if (eq (cdr (assq (char-before) skeletons-alist)) (char-after))
+;;       (and (char-after) (delete-char 1))))
+
+;; (defadvice backward-kill-word (around delete-pair activate)
+;;   (if (eq (char-syntax (char-before)) ?\()
+;;       (progn
+;; 	(backward-char 1)
+;; 	(save-excursion
+;; 	  (forward-sexp 1)
+;; 	  (delete-char -1))
+;; 	(forward-char 1)
+;; 	(append-next-kill)
+;; 	(kill-backward-chars 1))
+;;     ad-do-it))
+
+
+;; C/C++ skeleton commands
+;; for loop
+(define-skeleton c++-for-skel
+  "Insert a for loop in `c++-mode'." nil
+  "for (" > _ ")" > \n 
+  ?\{ > \n
+  > _ \n
+  ?} > )
+
+;; if conditional
+(define-skeleton c++-if-skel
+  "Insert if block in `c++-mode'." nil
+  "if (" > _ ")" > \n 
+  ?\{ > \n
+  > _ \n
+  ?} > )
+
+
+;;; abbreviation customisations
+
+;; expand abbrevs in context
+(defun expand-abbrev-in-context (expand)
+  "Expands abbreviations according to the context. Determines whether within
+comments or source by looking at the face name. If within comments the
+`text-mode-abbrev-table' is used, the major mode abbrev-table is used otherwise.
+
+Expansion is done by the function passed as the argument. This is controlled by
+the \"abnormal\" hook `abbrev-expand-functions'."
+;; backward-char checks if end-of-buffer as when point at e-o-b face is `nil'
+;; the function call expand does the expansion, usually `expand-abbrev'
+  (if (not (save-excursion
+	     (string-match "comment\\|string"
+			   (symbol-name (if (< (point) (point-max))
+					    (face-at-point)
+					  (backward-char)
+					  (face-at-point))))))
+      (funcall expand)
+    (let ((local-abbrev-table basic-text-mode-abbrev-table))
+      (funcall expand))))
+
+
+;;; end of skeletons.el
