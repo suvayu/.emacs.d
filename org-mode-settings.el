399b42caacc6e6198ff6a6bc5cb3ed68f024de1e
Suvayu Ali <Suvayu.Ali@cern.ch>
2010-08-09 10:58:39 -0700

    Added global key bind to switch to org buffers.

--- org-mode-settings.el
+++ org-mode-settings.el
@@ -91,6 +91,7 @@
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c l") 'org-store-link)
 (global-set-key (kbd "C-c f") 'org-footnote-action)
+(global-set-key (kbd "C-c b") 'org-switchb)
 
 ;; `org-mode' keymaps
 (defun my-org-mode-keymap()
