0f2f613b2b9f8208d62ea8f9b32da789e1fa497f
Suvayu Ali <fatkasuvayu+vc@gmail.com>
2010-10-09 18:50:38 -0700

    Some nifty lisp functions.
    1. make-GRL(region) - converts GRL region to if..else blocks

---
+++ nifty.el
@@ -0,0 +1,30 @@
+;; -*- mode: emacs-lisp; -*-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; This file contains some nifty lisp   ;;
+;; functions I wrote for my convenience ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; make ATLAS GoodRunList
+(defun make-GRL (beg end)
+  "Convert the marked region of a ATLAS GoodRunList
+XML to C if..else source blocks."
+  (interactive "r")
+  (if (> beg end) (let (mid) (setq mid beg beg end end mid)))
+  (save-excursion
+    (goto-char beg)
+    (while (re-search-forward
+	 "\\(<LumiBlockCollection>\n.+<Run>\\)\\([0-9]+\\)\\(</Run>\\)" nil t)
+      (replace-match "if ( Run == \\2 ) {" nil nil))
+    (goto-char beg)
+    (while (re-search-forward
+	    "\\(^ +\\)</LumiBlockCollection>"
+	    nil t)
+      (replace-match "\\1  return false;\n\\1}" nil nil))
+    (goto-char beg)
+    (while (re-search-forward
+	    "\\(<LBRange Start=\"\\)\\([0-9]+\\)\\(\" End=\"\\)\\([0-9]+\\)\\(\"/>\\)"
+	    nil t)
+      (replace-match "if ( LumiBlock >= \\2 && LumiBlock <= \\4) return true;" nil nil))
+    (remove-overlays beg end)
+    (message "Coverted GRL XML to C if..else blocks.")
+    ))
