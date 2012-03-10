74288bbea38ae70fcd3ed1858c588f22a1dd2e46
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2012-03-10 20:44:04 +0100

    Breaking change(8/8): Modularise emacs setup

--- init.el
+++ init.el
@@ -1,18 +1,5 @@
 ;; -*- mode: emacs-lisp -*-
-;;; .emacs --- my uber kewl .emacs on bhishma.homelinux.net
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; libraries in use:							 ;;
-;; 	dired-details, dired-details+,					 ;;
-;; 	dired-sort-menu, dired-sort-menu+, dired-sort-map		 ;;
-;; libraries loaded as required:					 ;;
-;; 	cedet, semantic, easist						 ;;
-;; libraries ready to use:						 ;;
-;; 	auto-install   						         ;;
-;; 									 ;;
-;; NB:	the fn to set $ROOTSYS works only for network mounted		 ;;
-;; 	directories. have to append user@host in front to make		 ;;
-;; 	it work for all situations					 ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; .emacs --- my uber kewl .emacs on bhishma
 
 ;;; Code:
 (setq debug-on-error t)
@@ -28,9 +15,9 @@
 (add-to-list 'load-path (expand-file-name "~/build/org-mode/contrib/lisp"))
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/lhcb"))
 
-;; ;; Info directory
-;; (add-to-list 'Info-default-directory-list
-;; 	     (expand-file-name "/opt/emacs-lisp/share/info"))
+;; Info directory
+(add-to-list 'Info-default-directory-list
+	     (expand-file-name "/opt/emacs-lisp/share/info"))
 
 ;; Emacs C source directory
 (setq find-function-C-source-directory "~/build/emacs/src")
@@ -94,6 +81,7 @@
  '(safe-local-variable-values (quote ((org-latex-to-pdf-process "xelatex -interaction nonstopmode -output-directory %o %f" "xelatex -interaction nonstopmode -output-directory %o %f" "xelatex -interaction nonstopmode -output-directory %o %f") (default-input-method . TeX) (org-export-allow-BIND . t))))
  '(save-abbrevs nil)
  '(savehist-mode t nil (savehist))
+ '(semantic-mode t)
  '(sentence-end-double-space nil)
  '(session-set-file-name-exclude-regexp "[/\\]\\.overview\\|[/\\]\\.session\\|News[/\\]\\|root.*/include/.+\\|/usr/include/.+\\|~/.mozilla.*itsalltext.*\\|.+\\.eml")
  '(session-use-package t nil (session))
@@ -110,211 +98,51 @@
  '(windmove-wrap-around t))
 
 
-;; colour themes using color-theme.el
-(load-file "~/.emacs.d/lisp/color-theme-dark-emacs.el")
+;; Colour theme and other gui related config
+(load-file "~/.emacs.d/gui-config.el")	; requires color-theme.el
 
-(eval-after-load "color-theme"
-  (progn
-    (setq color-theme-is-global nil)
-    (when (window-system) ; needed for the first frame
-      (color-theme-dark-emacs)
-      )))
-
-(add-hook 'after-make-frame-functions
-	  '(lambda (f)
-	     (with-selected-frame f
-	       (if (window-system f)
-		   (color-theme-dark-emacs)
-		 ))))
-
-;; ;; alternate theming API, supported by native Emacs >=24
-;; ;; colour theme using internal emacs theming
-;; (load-theme 'dark-emacs)
-
-;; (when (window-system) ; needed for the first frame
-;;   (load-theme 'dark-emacs))
-
-;; (add-hook 'after-make-frame-functions
-;; 	  '(lambda (f)
-;; 	     (with-selected-frame f
-;; 	       (if (window-system f)
-;; 		   (load-theme 'dark-emacs)
-;; 		 ))))
-
-
-;; hostname and buffer-name in frame title
-;; code originally written by Michael Albinus
-;; and a post in emacs-fu.blogspot.com (dotemacs trickery)
-(setq-default frame-title-format
-	      '(:eval
-		(if (string-match-p "^\\*.+\\*$" (buffer-name)) "%b" ; buffer name
-		  (format "%s:%s"
-			  (or (file-remote-p default-directory 'host) system-name)
-			  (buffer-name)))))
-		  ;; (format "%s@%s:%s"
-		  ;; 	(or (file-remote-p default-directory 'user) user-login-name)
-
-
-;; `minimal-mode' customisation
-(load-library "minimal")
-(setq minimal-zap-mode-line nil)
-(setq minimal-zap-menu-bar nil)
-(minimal-mode)
-
-
-;; Navigation
-;; side scrolling on
-(put 'scroll-left 'disabled nil)
-;; narrow-to-region enabled
-(put 'narrow-to-region 'disabled nil)
-;; navigate thru windows using M-<arrow>
-(windmove-default-keybindings 'meta)
-
-;; minibuffer history completion
-(mapc
-    '(lambda (map)
-       (define-key map [(meta p)] 'previous-complete-history-element)
-       (define-key map [(meta n)] 'next-complete-history-element))
-    (nconc (list minibuffer-local-completion-map
-                 minibuffer-local-isearch-map
-                 minibuffer-local-map
-                 minibuffer-local-must-match-map
-		 minibuffer-local-ns-map)))
-
-;; `occur-mode' customisations
-(define-key occur-mode-map (kbd "TAB") 'occur-mode-display-occurrence)
-
-
-;; Editing
-;; prefer utf-8
-(prefer-coding-system 'utf-8)
-;; enabling disabled commands
-(put 'downcase-region 'disabled nil)
-(put 'upcase-region 'disabled nil)
-;; tabs with respect to the previous non-blank line
-;; (define-key global-map (kbd "S-<iso-lefttab>") 'indent-relative)
-(define-key global-map (kbd "<backtab>") 'indent-relative)
-;; (global-set-key '[(backtab)] 'indent-relative)
-
-;; undo-tree (not in vanilla Emacs)
-(require 'undo-tree)
-(global-undo-tree-mode)
-
-;; ;; browse-kill-ring (not in vanilla Emacs)
-;; (require 'browse-kill-ring)
-;; (browse-kill-ring-default-keybindings)
-
-;; kill-ring-search
-(autoload 'kill-ring-search "kill-ring-search"
- "Search the kill ring in the minibuffer."
- (interactive))
-(global-set-key "\M-\C-y" 'kill-ring-search)
-
-;; ;; FIXME:
-;; (defadvice isearch-yank-kill
-;; (around toggle-case-fold-search-maybe)
-;; "If case-fold-search is t toggle it. Restore it after isearch finishes."
-;; (let ((tmp-case-fold-search case-fold-search))
-;;   (if case-fold-search
-;;       (progn (setq case-fold-search nil)
-;; 	     ad-do-it)
-;;     ad-do-it)
-;;   (setq case-fold-search tmp-case-fold-search)))
-;; (ad-activate 'isearch-yank-kill)
-
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Emacs pimped up! (development tools)			   ;;
-;; Collection of Emacs Development Environment Tools       ;;
-;; Skeletons, Templates, Abbreviations and Keyboard Macros ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; Semantic built into Emacs
-(semantic-mode 1)
-(semantic-add-system-include "/usr/include/root/" 'c++-mode)
-
-(add-to-list 'semantic-inhibit-functions
-	     (lambda () (not (member major-mode '(c-mode c++-mode)))))
-
-(add-hook 'c-mode-common-hook
-	  (lambda ()
-	    (define-key c-mode-base-map (kbd "C-c ?") 'semantic-ia-complete-symbol)
-	    (define-key c-mode-base-map (kbd "C-c t") 'semantic-ia-complete-tip)
-	    (define-key c-mode-base-map (kbd "C-c v") 'semantic-ia-show-variants)
-	    (define-key c-mode-base-map (kbd "C-c d") 'semantic-ia-show-doc)
-	    (define-key c-mode-base-map (kbd "C-c s") 'semantic-ia-show-summary)))
-
-;; ;; Load CEDET (do not move later, conflicts with eieio bundled with Emacs 24)
-;; (load-file "~/.emacs.d/lisp/cedet-configs.el")
-
-;; Skeletons (and abbrev customisations)
-(load-file "~/.emacs.d/lisp/skeletons.el")
-
-;; rebind as c-electric overrides global binding
-(add-hook 'c-mode-common-hook
-	  (lambda ()
-	    (local-set-key "(" 'skeleton-pair-insert-maybe)
-	    (local-set-key "{" 'skeleton-pair-insert-maybe)
-	    (local-set-key (kbd "M-RET") 'newline-and-indent)
-	    ;; (c-toggle-auto-newline 1)
-	    ))
-
-;; Abbreviations
-;; this hook wraps around the `expand-abbrev' function call
-(add-hook 'after-change-major-mode-hook
-	  (lambda ()
-	    (add-hook 'abbrev-expand-functions 'expand-abbrev-in-context nil t)))
-
+;; Important movement and editing config with some essential libraries
+(load-file "~/.emacs.d/keybindings.el")
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Mode hooks and other mode specific customisations ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; C/C++
+(load-file "~/.emacs.d/cpp-config.el")
+
 ;; `text-mode' and `org-mode' madness ;)
-(load-file "~/.emacs.d/lisp/org-mode-settings.el")
-(load-file "~/.emacs.d/lisp/text-mode-like-settings.el")
+(load-file "~/.emacs.d/org-mode-config.el")
+(load-file "~/.emacs.d/text-mode-config.el") ; has some org dependence
 
-;; (add-hook 'muse-mode-hook 'my-text-mode-hook)
-(add-hook 'text-mode-hook 'my-text-mode-hook)
-;; File associations (email)
-(add-to-list 'auto-mode-alist '("/mutt-" . message-mode)) ; mutt
-(add-to-list 'auto-mode-alist '("/tmpmsg." . message-mode)) ; claws
-(add-to-list 'auto-mode-alist (cons "\\.eml\\'" 'message-mode)) ; GMail w/ "It's all text!"
+;; use with mutt, ItsAllText and eml files
+(load-file "~/.emacs.d/email-config.el") ; has some org dependence
 
-;; w3m
-(add-hook 'w3m-mode-hook 'my-w3m-mode-hook)
+;; Lisp/Elisp customisations
+(defun sa-lisp-mode-hook ()
+  (eldoc-mode t))
+(add-hook 'lisp-mode-hook 'sa-lisp-mode-hook)
+(add-hook 'emacs-lisp-mode-hook 'sa-lisp-mode-hook)
+
+;; Python customisations
+
+
+;; w3m key bindings
+(defun sa-w3m-mode-hook ()
+  "Set up some w3m tabbed browsing key bindings."
+  (toggle-truncate-lines t)
+  (define-key w3m-mode-map (kbd "C-<tab>") 'w3m-next-buffer)
+  (define-key w3m-mode-map (kbd "C-<backtab>") 'w3m-previous-buffer)
+  (define-key w3m-mode-map (kbd "C-S-<iso-lefttab>") 'w3m-previous-buffer)
+  (define-key w3m-mode-map (kbd "s-<tab>") 'w3m-select-buffer))
+(add-hook 'w3m-mode-hook 'sa-w3m-mode-hook)
 
 ;; shell-script-mode customisations
 (add-hook 'after-save-hook 'executable-make-buffer-file-executable-if-script-p)
 
 ;; ANSI colours in Emacs shell
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
-
-;; font-lock customisations
-(add-hook 'find-file-hook
-	  (lambda ()
-	    (font-lock-add-keywords
-	     nil '(("\\<\\(FIXME\\):" 1 font-lock-warning-face prepend)))
-	    (font-lock-add-keywords
-	     nil '(("\\<\\(TODO\\):" 1 (org-get-todo-face "TODO") prepend)))))
-
-;; version control related customisations
-;; auto-revert-mode for files under version control
-(add-hook 'find-file-hook
-	  (lambda ()
-	    (if (vc-working-revision (buffer-file-name)) ; (egg-buf-git-name)
-		(auto-revert-mode t))
-	    ))
-
-;; mode to edit git commit message
-(autoload 'git-commit-mode "git-commit"
-  "Major mode for editing git commit messages." t)
-
-(add-to-list 'auto-mode-alist '("COMMIT_EDITMSG" . git-commit-mode))
-(add-hook 'git-commit-mode-hook
-	  (lambda () (turn-on-orgstruct++)))
 
 ;; Emacs Got Git (git frontend, magit fork)
 ;; (require 'egg)
@@ -336,58 +164,22 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; onyanyo jhinchak stuff:			;;
-;; auto-install, dired-details, dired-details+, ;;
-;; icicles, dired-sort-menu, dired-sort-menu+	;;
+;; auto-install                                 ;;
+;; icicles                                      ;;
+;; auto-complete                                ;;
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; ;; help enhancements by Drew Adams
+;; (require 'help-mode+)
+;; (require 'help+)
+;; (require 'help-fns+)
 
 ;; auto-install settings (not in vanilla Emacs)
 (eval-after-load 'auto-install
   (setq auto-install-directory "~/.emacs.d/lisp/"))
 ;; the "/" at the end of the path is absolutely essential,
 ;; otherwise the elisp files are saved as elisp*.el instead of elisp/*.el
-
-
-;; dired
-(put 'dired-find-alternate-file 'disabled nil)
-
-;; `dired-details' & `dired-details+' by Drew Adams
-;; reported a bug for this 1 ;) (not in vanilla Emacs)
-(require 'dired-details)
-(require 'dired-details+)
-(require 'dired-sort-menu)
-(require 'dired-sort-menu+)
-(require 'dired-sort-map)
-
-;; ;; load dired-x in dired (enables omitting files)
-;; (add-hook 'dired-load-hook
-;;	  (lambda ()
-;;	    (require 'dired-x)))
-;; ;; turns omiting on by default
-;; (setq dired-omit-mode t)
-
-;; `dired-mode' key bindings
-(define-key dired-mode-map (kbd "C-<down>") 'dired-next-subdir)
-(define-key dired-mode-map (kbd "C-<up>") 'dired-prev-subdir)
-(define-key dired-mode-map (kbd "C-<left>") 'dired-tree-up)
-(define-key dired-mode-map (kbd "C-<right>") 'dired-tree-down)
-(define-key dired-mode-map (kbd "<tab>") 'dired-hide-subdir)
-
-
-;; lazy-bones
-(defalias 'yes-or-no-p 'y-or-n-p)
-;; session management (not in vanilla Emacs)
-(require 'session)
-(add-hook 'after-init-hook 'session-initialize)
-;; coding system to use when writing `session-save-file'
-(setq session-save-file-coding-system 'utf-8)
-
-;; Show clock in the modeline
-(display-time-mode 1)
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Development tools: Pymacs ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; auto-complete
 (require 'auto-complete-config)
@@ -404,48 +196,26 @@
 	    (add-to-list 'ac-sources 'ac-source-math-latex)))
 (add-to-list 'ac-modes 'latex-mode)
 
-;; Python customisations
 
-;; Pymacs setup
-(autoload 'pymacs-apply "pymacs")
-(autoload 'pymacs-call "pymacs")
-(autoload 'pymacs-eval "pymacs" nil t)
-(autoload 'pymacs-exec "pymacs" nil t)
-(autoload 'pymacs-load "pymacs" nil t)
-;;(eval-after-load "pymacs"
-;;  '(add-to-list 'pymacs-load-path YOUR-PYMACS-DIRECTORY"))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Development tools: Pymacs ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-
-;; Lisp/Elisp customisations
-(defun my-lisp-mode-hook ()
-  (eldoc-mode t))
-(add-hook 'lisp-mode-hook 'my-lisp-mode-hook)
-(add-hook 'emacs-lisp-mode-hook 'my-lisp-mode-hook)
+;; ;; Pymacs setup
+;; (autoload 'pymacs-apply "pymacs")
+;; (autoload 'pymacs-call "pymacs")
+;; (autoload 'pymacs-eval "pymacs" nil t)
+;; (autoload 'pymacs-exec "pymacs" nil t)
+;; (autoload 'pymacs-load "pymacs" nil t)
+;; ;;(eval-after-load "pymacs"
+;; ;;  '(add-to-list 'pymacs-load-path YOUR-PYMACS-DIRECTORY"))
 
 
-;; C++ customisations
-;; force `c++-mode' for `*.h' header files
-(add-to-list 'auto-mode-alist (cons "\\.h\\'" 'c++-mode))
 
-;; keybindings
-;; (defun c-mode-common-keymaps()
-;;   "C/C++ mode keybindings for my skeletons."
-;;   ;; keybindings for skeletons
-;;   (define-key c-mode-base-map (kbd "C-c f") 'c++-for-skel)
-;;   (define-key c-mode-base-map (kbd "C-c i") 'c++-if-skel))
+;;;;;;;;;;;;;;;;;;;;;;;;
+;; HEP specific modes ;;
+;;;;;;;;;;;;;;;;;;;;;;;;
 
-
-;; hooks
-;; (add-hook 'c-mode-common-hook 'c-mode-common-keymaps)
-
-;; Documentation tools
-;; doxymacs
-(require 'doxymacs)
-;; (eval-after-load "doxymacs"
-;;   (define-key c-mode-base-map '[(C-c d)] doxymacs-))
-
-
-;; HEP specific modes
 ;; `cmt-mode' for CMT requirements files
 (autoload 'cmt-mode "cmt-mode"
   "Mode to fontify and syntax highlight buffer while editing
@@ -461,7 +231,6 @@
 ;;   "Major mode for editing HAN configuration files" t)
 ;; (add-to-list 'auto-mode-alist
 ;; 	     (cons "\\(run\\|minutes[0-9]\\{1,3\\}\\).config\\'" 'han-mode))
-
 
 
 ;;; .emacs ends here
