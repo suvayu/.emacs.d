690343810f4109d934b6df6bf0f4f05781dcdd02
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2012-04-28 14:04:33 +0200

    Turn off color-theme library
    The commit below in Emacs 24 removed a deprecated form. This broke
    color-theme, so switching to Emacs theming.
    
     commit 2faa2c5d69f36c29cdd3b654f4cc9c4b89c81e5a
     Author: Chong Yidong <cyd@gnu.org>
     Date:   Mon Apr 23 02:04:54 2012 +0800
    
         * faces.el (face-spec-set): Stop supporting deprecated form of
           third arg.

--- gui-config.el
+++ gui-config.el
@@ -1,19 +1,27 @@
 ;; colour themes using color-theme.el
-(load-file "~/.emacs.d/lisp/color-theme-dark-emacs.el")
+;;
+;; Doesn't work since the following commit in Emacs 24
+;; commit 2faa2c5d69f36c29cdd3b654f4cc9c4b89c81e5a
+;; Author: Chong Yidong <cyd@gnu.org>
+;; Date:   Mon Apr 23 02:04:54 2012 +0800
+;;
+;;     * faces.el (face-spec-set): Stop supporting deprecated form of third arg.
+;;
+;; (load-file "~/.emacs.d/lisp/color-theme-dark-emacs.el")
 
-(eval-after-load "color-theme"
-  (progn
-    (setq color-theme-is-global nil)
-    (when (window-system) ; needed for the first frame
-      (color-theme-dark-emacs)
-      )))
+;; (eval-after-load "color-theme"
+;;   (progn
+;;     (setq color-theme-is-global nil)
+;;     (when (window-system) ; needed for the first frame
+;;       (color-theme-dark-emacs)
+;;       )))
 
-(add-hook 'after-make-frame-functions
-	  '(lambda (f)
-	     (with-selected-frame f
-	       (if (window-system f)
-		   (color-theme-dark-emacs)
-		 ))))
+;; (add-hook 'after-make-frame-functions
+;; 	  '(lambda (f)
+;; 	     (with-selected-frame f
+;; 	       (if (window-system f)
+;; 		   (color-theme-dark-emacs)
+;; 		 ))))
 
 ;; ;; alternate theming API, supported by native Emacs >=24
 ;; ;; colour theme using internal emacs theming
@@ -21,13 +29,6 @@
 
 ;; (when (window-system) ; needed for the first frame
 ;;   (load-theme 'dark-emacs))
-
-;; (add-hook 'after-make-frame-functions
-;; 	  '(lambda (f)
-;; 	     (with-selected-frame f
-;; 	       (if (window-system f)
-;; 		   (load-theme 'dark-emacs)
-;; 		 ))))
 
 
 ;; hostname and buffer-name in frame title
