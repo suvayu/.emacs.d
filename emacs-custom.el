5dac588ab16766701bfe957b9c433c1c75ea6bf0
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2012-07-02 00:49:31 +0200

    Modularise: move customize to separate file

---
+++ emacs-custom.el
@@ -0,0 +1,82 @@
+;; -*- mode: emacs-lisp -*-
+;;; Emacs custom file
+
+;; faces
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(completions-common-part ((t (:foreground "forest green"))))
+ '(completions-first-difference ((t (:bold t :weight bold :foreground "salmon"))))
+ '(font-lock-warning-face ((((type tty)) (:inherit error :weight bold))))
+ '(info-menu-header ((t (:bold t :family "Sans Serif" :foreground "tomato" :weight bold))))
+ '(info-node ((t (:italic t :bold t :foreground "gold" :slant italic :weight bold))))
+ '(info-xref ((t (:inherit link :foreground "powder blue" :weight bold))))
+ '(info-xref-visited ((t (:foreground "violet" :underline t :weight bold))))
+ '(link ((t (:foreground "cyan" :underline t :weight extra-bold))))
+ '(minibuffer-prompt ((t (:foreground "dark cyan" :weight bold))))
+ '(org-agenda-current-time ((t (:inherit org-time-grid :background "snow" :foreground "DodgerBlue4" :weight bold))) t)
+ '(org-done ((t (:background "ForestGreen" :foreground "DarkSeaGreen2" :slant oblique :weight bold))))
+ '(org-inlinetask ((t (:inherit org-level-8 :slant oblique))))
+ '(org-level-3 ((t (:inherit outline-3 :foreground "sandy brown" :weight bold))))
+ '(org-todo ((t (:background "royalblue4" :foreground "thistle" :weight bold))))
+ '(rst-level-1-face ((t (:background "grey85" :foreground "black"))) t)
+ '(woman-bold ((t (:bold t :weight bold :foreground "forest green"))) t)
+ '(woman-italic ((t (:italic t :slant italic :foreground "salmon"))) t))
+
+;; if $TERM=xterm-256color
+;; '(mode-line ((t (:background "brightwhite" :foreground "black" :box (:line-width -1 :style released-button)))))
+
+
+;; configs
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(Info-additional-directory-list (quote ("~jallad/.emacs.d/info-manuals")))
+ '(abbrev-mode t t)
+ '(browse-url-browser-function (quote (("http.*emacswiki.org/.*" . w3m-browse-url) ("." . browse-url-default-browser))))
+ '(calendar-date-style (quote iso))
+ '(case-fold-search nil)
+ '(column-number-mode t)
+ '(custom-enabled-themes (quote (dark-emacs)))
+ '(custom-safe-themes (quote ("9d04c19fa8d227f59234596060d285282599de77e0ee4f418aae5191a3bbf6bd" default)))
+ '(custom-theme-directory "~/.emacs.d/themes/")
+ '(dabbrev-case-replace nil)
+ '(default-input-method "TeX")
+ '(diff-switches "-u")
+ '(dired-listing-switches "-alh")
+ '(ediff-custom-diff-options "-u")
+ '(ediff-split-window-function (quote split-window-horizontally))
+ '(ediff-window-setup-function (quote ediff-setup-windows-plain))
+ '(egg-buffer-hide-section-type-on-start (quote ((egg-status-buffer-mode . :hunk) (egg-commit-buffer-mode . :hunk))))
+ '(ibuffer-mode-hook (quote (ibuffer-auto-mode)))
+ '(inhibit-startup-screen t)
+ '(isearch-allow-scroll t)
+ '(ispell-dictionary "british")
+ '(iswitchb-mode t)
+ '(mouse-avoidance-mode (quote exile) nil (avoid))
+ '(mouse-yank-at-point t)
+ '(notmuch-saved-searches (quote (("Inbox-unread" . "tag:inbox and is:unread") ("NIKHEF" . "tag:nikhef") ("CERN" . "tag:cern") ("lists" . "tag:list") ("Bfys" . "tag:bfys") ("Orgmode" . "tag:org") ("Inbox" . "tag:inbox") ("unread" . "tag:unread"))))
+ '(occur-mode-hook (quote (turn-on-font-lock next-error-follow-minor-mode)))
+ '(package-archives (quote (("gnu" . "http://elpa.gnu.org/packages/") ("ELPA" . "http://tromey.com/elpa/") ("marmalade" . "http://marmalade-repo.org/packages/"))))
+ '(safe-local-variable-values (quote ((org-latex-to-pdf-process "xelatex -interaction nonstopmode -output-directory %o %f" "xelatex -interaction nonstopmode -output-directory %o %f" "xelatex -interaction nonstopmode -output-directory %o %f") (default-input-method . TeX) (org-export-allow-BIND . t))))
+ '(save-abbrevs nil)
+ '(savehist-mode t nil (savehist))
+ '(semantic-mode t)
+ '(sentence-end-double-space nil)
+ '(session-set-file-name-exclude-regexp "[/\\]\\.overview\\|[/\\]\\.session\\|News[/\\]\\|\\(/.+/include/.+\\|~/\\.mozilla.*itsalltext.*\\|.*COMMIT_EDITMSG\\)")
+ '(session-use-package t nil (session))
+ '(set-mark-command-repeat-pop t)
+ '(show-paren-mode t)
+ '(speedbar-use-images nil)
+ '(tramp-default-method "ssh")
+ '(transient-mark-mode t)
+ '(truncate-lines t)
+ '(uniquify-buffer-name-style (quote post-forward) nil (uniquify))
+ '(user-mail-address "Suvayu.Ali@cern.ch")
+ '(vc-handled-backends (quote (Git RCS CVS SVN SCCS Bzr Hg Mtn Arch)))
+ '(w3m-use-cookies t)
+ '(windmove-wrap-around t))
--- minimal-commit.el
+++ minimal-commit.el
@@ -1,67 +1,14 @@
+;; -*- mode: emacs-lisp -*-
+;;; minimal-commit.el
+;; Minimal settings for making git commits
+
 ;; load path for elisp files
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
 
+;; load custom file
+(setq custom-file "~jallad/.emacs.d/emacs-custom.el")
+(load-file custom-file)
 
-;; faces
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(completions-common-part ((t (:foreground "forest green"))))
- '(completions-first-difference ((t (:bold t :weight bold :foreground "salmon"))))
- '(font-lock-warning-face ((((type tty)) (:inherit error :weight bold))))
- '(info-menu-header ((t (:bold t :family "Sans Serif" :foreground "tomato" :weight bold))))
- '(info-node ((t (:italic t :bold t :foreground "gold" :slant italic :weight bold))))
- '(info-xref ((t (:inherit link :foreground "powder blue" :weight bold))))
- '(info-xref-visited ((t (:foreground "violet" :underline t :weight bold))))
- '(minibuffer-prompt ((t (:foreground "dark cyan" :weight bold))))
- '(org-todo ((t (:background "royalblue4" :foreground "thistle" :weight bold))))
- '(rst-level-1-face ((t (:background "grey85" :foreground "black"))) t)
- '(woman-bold ((t (:bold t :weight bold :foreground "forest green"))))
- '(woman-italic ((t (:italic t :slant italic :foreground "salmon")))))
-
-
-;; configs
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(abbrev-mode t t)
- '(calendar-date-style (quote iso))
- '(case-fold-search nil)
- '(column-number-mode t)
- '(dabbrev-case-replace nil)
- '(diff-switches "-u")
- '(dired-listing-switches "-alh")
- '(ediff-custom-diff-options "-u")
- '(ediff-split-window-function (quote split-window-horizontally))
- '(ediff-window-setup-function (quote ediff-setup-windows-plain))
- '(ibuffer-mode-hook (quote (ibuffer-auto-mode)))
- '(inhibit-startup-screen t)
- '(isearch-allow-scroll t)
- '(ispell-dictionary "british")
- '(iswitchb-mode t)
- '(mouse-avoidance-mode (quote exile) nil (avoid))
- '(mouse-yank-at-point t)
- '(occur-mode-hook (quote (turn-on-font-lock next-error-follow-minor-mode)))
- '(save-abbrevs nil)
- '(savehist-mode t nil (savehist))
- '(sentence-end-double-space nil)
- '(session-set-file-name-exclude-regexp "[/\\]\\.overview\\|[/\\]\\.session\\|News[/\\]\\|root.*/include/.+\\|/usr/include/.+\\|~/.mozilla.*itsalltext.*\\|.+\\.eml")
- '(session-use-package t nil (session))
- '(set-mark-command-repeat-pop t)
- '(show-paren-mode t)
- '(speedbar-use-images nil)
- '(tramp-default-method "ssh")
- '(transient-mark-mode t)
- '(truncate-lines t)
- '(uniquify-buffer-name-style (quote post-forward) nil (uniquify))
- '(user-mail-address "Suvayu.Ali@cern.ch")
- '(vc-handled-backends (quote (Git RCS CVS SVN SCCS Bzr Hg Mtn Arch)))
- '(windmove-wrap-around t))
-
-
+;; keybindings and text-mode settings
 (load-file "~/.emacs.d/keybindings.el")
 (load-file "~/.emacs.d/text-mode-config.el")
