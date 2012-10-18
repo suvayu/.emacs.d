84b845229b762f910139b43c912eb70c86fde4e2
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2012-10-18 14:41:22 +0200

    Add abook like addressbook to emacs message mode
    Adds dependency on external-abook

--- email-config.el
+++ email-config.el
@@ -7,3 +7,8 @@
 (add-to-list 'auto-mode-alist '("/mutt-" . message-mode)) ; mutt
 (add-to-list 'auto-mode-alist '("/tmpmsg." . message-mode)) ; claws
 (add-to-list 'auto-mode-alist '("\\.eml\\'" . message-mode)) ; GMail w/ "It's all text!"
+
+
+(load-library "external-abook")
+(setq external-abook-command "nottoomuch-addresses %s")
+(define-key message-mode-map (kbd "C-c C-SPC") 'external-abook-try-expand)
