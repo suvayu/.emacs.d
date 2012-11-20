7dc463fa1906d5c52f333710dbeca67875b6726b
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2012-11-20 16:11:03 +0100

    Temporarily remove git-commit-mode from init.el

--- init.el
+++ init.el
@@ -68,13 +68,17 @@
 		(auto-revert-mode t))
 	    ))
 
-;; mode to edit git commit message
-(autoload 'git-commit-mode "git-commit"
-  "Major mode for editing git commit messages." t)
+;; FIXME: When last tested, git-commit-mode was unstable.  It would
+;; crash Emacs with C-g in the terminal.  This makes it using
+;; emacsclient unsuitable for git commits from the command line.
 
-(add-to-list 'auto-mode-alist '("COMMIT_EDITMSG" . git-commit-mode))
-(add-hook 'git-commit-mode-hook
-	  (lambda () (turn-on-orgstruct++)))
+;; ;; mode to edit git commit message
+;; (autoload 'git-commit-mode "git-commit"
+;;   "Major mode for editing git commit messages." t)
+
+;; (add-to-list 'auto-mode-alist '("COMMIT_EDITMSG" . git-commit-mode))
+;; (add-hook 'git-commit-mode-hook
+;; 	  (lambda () (turn-on-orgstruct++)))
 
 ;; C/C++
 (load-file "~/.emacs.d/cpp-config.el")
--- minimal-commit.el
+++ minimal-commit.el
@@ -12,3 +12,13 @@
 ;; keybindings and text-mode settings
 (load-file "~/.emacs.d/keybindings.el")
 (load-file "~/.emacs.d/text-mode-config.el")
+
+;; FIXME: Temporarily until stability of git-commit-mode is tested again
+
+;; mode to edit git commit message
+(autoload 'git-commit-mode "git-commit"
+  "Major mode for editing git commit messages." t)
+
+(add-to-list 'auto-mode-alist '("COMMIT_EDITMSG" . git-commit-mode))
+(add-hook 'git-commit-mode-hook
+	  (lambda () (turn-on-orgstruct++)))
