694d8701efe7a5799256790c11ddf7a675e0a25a
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2013-02-27 17:29:13 +0100

    Remove minimal-commit
    Emacs does not crash with C-g in git-commit-mode any more.

--- vc-config.el
+++ vc-config.el
@@ -1,7 +1,7 @@
 ;; -*- mode: emacs-lisp; -*-
 ;;; vc-config.el
 
-;; ;; ;; light weight additions and remapping to vc-git (ELPA)
+;; ;; light weight additions and remapping to vc-git (ELPA)
 ;; (load-library "gitty")
 
 ;; auto-revert-mode for files under version control
@@ -14,14 +14,8 @@
 		  ))
 	    ))
 
-;; FIXME: When last tested, git-commit-mode was unstable.  It would
-;; crash Emacs with C-g in the terminal.  This makes it using
-;; emacsclient unsuitable for git commits from the command line.
-
-;; ;; mode to edit git commit message
-;; (autoload 'git-commit-mode "git-commit"
-;;   "Major mode for editing git commit messages." t)
-
-;; (add-to-list 'auto-mode-alist '("COMMIT_EDITMSG" . git-commit-mode))
-;; (add-hook 'git-commit-mode-hook
-;; 	  (lambda () (turn-on-orgstruct++)))
+;; mode to edit git commit message
+(autoload 'git-commit-mode "git-commit"
+  "Major mode for editing git commit messages." t)
+(add-to-list 'auto-mode-alist '("COMMIT_EDITMSG" . git-commit-mode))
+(add-hook 'git-commit-mode-hook (lambda () (orgstruct++-mode t)))
