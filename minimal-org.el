5e8d58daa10e7e50b3915d90a114bc3c477193ed
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2013-03-03 17:17:17 +0100

    Add beamer to minimal org

--- minimal-org.el
+++ minimal-org.el
@@ -6,6 +6,13 @@
 (add-to-list 'load-path (expand-file-name "~/build/org-mode/lisp"))
 (add-to-list 'load-path (expand-file-name "~/build/org-mode/contrib/lisp"))
 
+(add-to-list 'org-latex-classes
+             '("beamer"
+               "\\documentclass\[presentation\]\{beamer\}"
+               ("\\section\{%s\}" . "\\section*\{%s\}")
+               ("\\subsection\{%s\}" . "\\subsection*\{%s\}")
+               ("\\subsubsection\{%s\}" . "\\subsubsection*\{%s\}")))
+
 ;; ;; Default: w/o bibtex
 ;; (setq org-latex-to-pdf-process
 ;;       '("pdflatex -interaction nonstopmode %b"
