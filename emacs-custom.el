076691a182aec65ef1f1d1f032589ee52349b484
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2013-06-09 21:28:09 +0200

    Nicely formatted custom-file (new in Emacs 24.3!)

--- emacs-custom.el
+++ emacs-custom.el
@@ -18,12 +18,17 @@
  '(Info-additional-directory-list (quote ("~/.emacs.d/info-manuals")))
  '(abbrev-mode t t)
  '(blink-cursor-mode t)
- '(browse-url-browser-function (quote (("http.*emacswiki.org/.*" . w3m-browse-url) ("." . browse-url-default-browser))))
+ '(browse-url-browser-function
+   (quote
+    (("http.*emacswiki.org/.*" . w3m-browse-url)
+     ("." . browse-url-default-browser))))
  '(calendar-date-style (quote iso))
  '(case-fold-search nil)
  '(column-number-mode t)
  '(custom-enabled-themes (quote (dark-emacs)))
- '(custom-safe-themes (quote ("7009c998e33503b344818bb66beed3fbb8a17238fb6f176493ef7f74b27f1f50" default)))
+ '(custom-safe-themes
+   (quote
+    ("7009c998e33503b344818bb66beed3fbb8a17238fb6f176493ef7f74b27f1f50" default)))
  '(custom-theme-directory "~/.emacs.d/themes/")
  '(dabbrev-case-replace nil)
  '(default-input-method "TeX")
@@ -33,10 +38,49 @@
  '(ediff-custom-diff-options "-u")
  '(ediff-split-window-function (quote split-window-horizontally))
  '(ediff-window-setup-function (quote ediff-setup-windows-plain))
- '(egg-buffer-hide-section-type-on-start (quote ((egg-status-buffer-mode . :hunk) (egg-commit-buffer-mode . :hunk))))
+ '(egg-buffer-hide-section-type-on-start
+   (quote
+    ((egg-status-buffer-mode . :hunk)
+     (egg-commit-buffer-mode . :hunk))))
  '(ibuffer-mode-hook (quote (ibuffer-auto-mode)))
  '(ibuffer-saved-filter-groups nil)
- '(ibuffer-saved-filters (quote (("rootpy" ((filename . "rootpy") (saved . "py"))) ("dsk" ((filename . "Bs2DsKTools") (saved . "py"))) ("dev" ((or (saved . "py") (saved . "cpp")))) ("lisp" ((used-mode . emacs-lisp-mode))) ("py" ((used-mode . python-mode))) ("config" ((used-mode . conf-unix-mode))) ("org" ((used-mode . org-mode))) ("cpp" ((used-mode . c++-mode))) ("gnus" ((or (mode . message-mode) (mode . mail-mode) (mode . gnus-group-mode) (mode . gnus-summary-mode) (mode . gnus-article-mode)))) ("programming" ((or (mode . emacs-lisp-mode) (mode . cperl-mode) (mode . c-mode) (mode . java-mode) (mode . idl-mode) (mode . lisp-mode)))))))
+ '(ibuffer-saved-filters
+   (quote
+    (("rootpy"
+      ((filename . "rootpy")
+       (saved . "py")))
+     ("dsk"
+      ((filename . "Bs2DsKTools")
+       (saved . "py")))
+     ("dev"
+      ((or
+	(saved . "py")
+	(saved . "cpp"))))
+     ("lisp"
+      ((used-mode . emacs-lisp-mode)))
+     ("py"
+      ((used-mode . python-mode)))
+     ("config"
+      ((used-mode . conf-unix-mode)))
+     ("org"
+      ((used-mode . org-mode)))
+     ("cpp"
+      ((used-mode . c++-mode)))
+     ("gnus"
+      ((or
+	(mode . message-mode)
+	(mode . mail-mode)
+	(mode . gnus-group-mode)
+	(mode . gnus-summary-mode)
+	(mode . gnus-article-mode))))
+     ("programming"
+      ((or
+	(mode . emacs-lisp-mode)
+	(mode . cperl-mode)
+	(mode . c-mode)
+	(mode . java-mode)
+	(mode . idl-mode)
+	(mode . lisp-mode)))))))
  '(inhibit-startup-screen t)
  '(isearch-allow-scroll t)
  '(ispell-dictionary "british")
@@ -49,21 +93,42 @@
  '(mouse-yank-at-point t)
  '(notmuch-address-command "nottoomuch-addresses")
  '(notmuch-always-prompt-for-sender t)
- '(notmuch-fcc-dirs (quote (("fatkasuvayu+linux@gmail.com" . "") (".\\+" . "sent"))))
+ '(notmuch-fcc-dirs
+   (quote
+    (("fatkasuvayu+linux@gmail.com" . "")
+     (".\\+" . "sent"))))
  '(notmuch-message-replied-tags (quote ("+replied")))
- '(notmuch-saved-searches (quote (("Inbox-unread" . "tag:inbox and is:unread") ("NIKHEF" . "tag:nikhef") ("CERN" . "tag:cern") ("Bfys" . "tag:bfys") ("Inbox" . "tag:inbox") ("unread" . "tag:unread"))))
+ '(notmuch-saved-searches
+   (quote
+    (("Inbox-unread" . "tag:inbox and is:unread")
+     ("NIKHEF" . "tag:nikhef")
+     ("CERN" . "tag:cern")
+     ("Bfys" . "tag:bfys")
+     ("Inbox" . "tag:inbox")
+     ("unread" . "tag:unread"))))
  '(notmuch-search-oldest-first nil)
  '(notmuch-show-all-multipart/alternative-parts nil)
  '(occur-mode-hook (quote (turn-on-font-lock next-error-follow-minor-mode)))
  '(org-export-backends (quote (ascii beamer html latex org)))
- '(package-archives (quote (("gnu" . "http://elpa.gnu.org/packages/") ("ELPA" . "http://tromey.com/elpa/") ("marmalade" . "http://marmalade-repo.org/packages/") ("melpa" . "http://melpa.milkbox.net/packages/"))))
- '(safe-local-variable-values (quote ((org-latex-to-pdf-process "xelatex -interaction nonstopmode -output-directory %o %f" "xelatex -interaction nonstopmode -output-directory %o %f" "xelatex -interaction nonstopmode -output-directory %o %f") (default-input-method . TeX) (org-export-allow-BIND . t))))
+ '(package-archives
+   (quote
+    (("gnu" . "http://elpa.gnu.org/packages/")
+     ("ELPA" . "http://tromey.com/elpa/")
+     ("marmalade" . "http://marmalade-repo.org/packages/")
+     ("melpa" . "http://melpa.milkbox.net/packages/"))))
+ '(safe-local-variable-values
+   (quote
+    ((local-abbrev-table . latex-mode-abbrev-table)
+     (org-latex-to-pdf-process "xelatex -interaction nonstopmode -output-directory %o %f" "xelatex -interaction nonstopmode -output-directory %o %f" "xelatex -interaction nonstopmode -output-directory %o %f")
+     (default-input-method . TeX)
+     (org-export-allow-BIND . t))))
  '(save-abbrevs nil)
  '(savehist-mode t nil (savehist))
  '(semantic-mode t)
  '(send-mail-function (quote smtpmail-send-it))
  '(sentence-end-double-space t)
- '(session-set-file-name-exclude-regexp "\\.session\\|News/\\|/\\(usr\\|opt/.+\\)/include/\\|~/\\.mozilla.*itsalltext.*\\|.*COMMIT_EDITMSG\\|/tmp/\\(mutt-\\|alot\\.\\).*")
+ '(session-set-file-name-exclude-regexp
+   "\\.session\\|News/\\|/\\(usr\\|opt/.+\\)/include/\\|~/\\.mozilla.*itsalltext.*\\|.*COMMIT_EDITMSG\\|/tmp/\\(mutt-\\|alot\\.\\).*")
  '(session-use-package t nil (session))
  '(set-mark-command-repeat-pop t)
  '(show-paren-mode t)
