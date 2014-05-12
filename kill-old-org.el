8c668cd28007356afb733238f1dde4eb0e0e4c46
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2014-05-12 20:24:35 +0200

    Update kill-old-org.el

--- kill-old-org.el
+++ kill-old-org.el
@@ -1,18 +1,21 @@
 ;;
 ;; Kill any old org remnants (thanks to Achim)
 ;;
+;; From: org-mode.git/testing/org-batch-test-init.el
 
 ;; clean load-path
 (setq load-path
       (delq nil (mapcar
 		 (function (lambda (p)
-			     (unless (string-match "lisp/org$" p)
+			     (unless (string-match "lisp\\(/packages\\)?/org$" p)
 			       p)))
 		 load-path)))
-;; remove property lists to defeat cus-load and remove autoloads
+;; remove property list to defeat cus-load and remove autoloads
 (mapatoms (function  (lambda (s)
 		       (let ((sn (symbol-name s)))
-			 (when (string-match "^\\(org\\|ob\\|ox\\)-?" sn)
+			 (when (string-match "^\\(org\\|ob\\|ox\\)\\(-.*\\)?$" sn)
 			   (setplist s nil)
-			   (when (autoloadp s)
+			   (when (eq 'autoload (car-safe s))
 			     (unintern s)))))))
+
+;; we should now start from a clean slate
