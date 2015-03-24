ef1dc5f625fe659c8ce5d1199d35a9758a9437a8
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2015-03-24 11:42:39 +0100

    Update how auto-revert-mode is activated
    Latest Emacs throws error when vc-working-revision is called on an
    unversioned file.  Refactor the check to catch and proceed silently.

--- vc-config.el
+++ vc-config.el
@@ -7,12 +7,10 @@
 ;; auto-revert-mode for files under version control
 (add-hook 'find-file-hook
 	  (lambda ()
-	    (if (vc-working-revision (buffer-file-name)) ; (egg-buf-git-name)
-		(progn
-		  (auto-revert-mode t)
-		  ;; (eval-after-load 'gitty (gitty-mode 1))
-		  ))
-	    ))
+	    (condition-case nil
+		(if (vc-working-revision (buffer-file-name))
+		    (auto-revert-mode t))
+	      (error nil))))
 
 ;; mode to edit git commit message
 (autoload 'git-commit-mode "git-commit"
