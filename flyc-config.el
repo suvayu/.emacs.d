09d3795c55f8ffdb2e24727b646de21a07acfcfa
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2019-07-06 23:58:01 +0530

    Add flycheck config (was missing)

---
+++ flyc-config.el
@@ -0,0 +1,26 @@
+;;; flyc-config.el --- Flycheck config
+
+;;; Commentary:
+
+;;; Code:
+(require 'flycheck)
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; many features are not supported well; e.g. openmp
+(delete 'c/c++-clang flycheck-checkers)
+
+(add-to-list 'flycheck-checkers 'python-flake8)
+
+;; navigate flycheck errors w/ hydra
+(defhydra hydra-flycerr (flycheck-command-map)
+  "goto-flycheck-error"
+  ("<down>" flycheck-next-error "next")
+  ("<up>" flycheck-previous-error "prev")
+  ("<space>" flycheck-list-errors "list")
+  ("q" nil "quit"))
+
+;;; flyc-config.el ends here
+
+;; Local Variables:
+;; flycheck-disabled-checkers: (emacs-lisp emacs-lisp-checkdoc)
+;; End:
