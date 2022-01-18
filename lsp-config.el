fca008fa0104bdac1a2c7f090e78ab530684dc91
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2022-01-18 17:03:21 +0100

    lsp-config.el: key binding for docs

--- lsp-config.el
+++ lsp-config.el
@@ -6,8 +6,13 @@
 (require 'lsp-mode)
 
 (with-eval-after-load 'lsp-ui
+  (define-key lsp-ui-mode-map [f7] #'lsp-ui-imenu)
   (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
   (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references))
+
+(with-eval-after-load 'lsp-mode
+  (define-key lsp-mode-map (kbd "C-c .") #'lsp-signature-activate)
+  (define-key lsp-mode-map (kbd "C-c d") #'lsp-describe-thing-at-point))
 
 ;;; lsp-config.el ends here
 
