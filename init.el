c002cd1c912dfbec6aec53a2531461bdd989d04a
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2022-01-18 17:11:00 +0100

    package.el & use-package setup

--- init.el
+++ init.el
@@ -3,6 +3,13 @@
 ;;; Commentary:
 
 ;;; Code:
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+(require 'use-package)
+
 (require 'cl-lib)
 (setq debug-on-error t)
       ;; debug-on-signal t
