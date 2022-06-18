3d4a10e03294da2294b4a3f8020e4f5a123edfee
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2022-06-18 13:44:18 +0200

    cpp-config.el: move to use-package

--- cpp-config.el
+++ cpp-config.el
@@ -3,52 +3,58 @@
 
 ;;; Code: C++ customisations
 
-;; associate arduino sketches to c-mode
-(add-to-list 'auto-mode-alist (cons "\\.ino\\'" 'c++-mode))
+(use-package c-mode
+  :mode ("\\.ino\\'" . c-mode)		;arduino sketches to c-mode
+  :hook
+  (c-mode-common . (lambda ()
+		     (flycheck-mode 1)
+		     ;; for semantic imenu
+		     (setq imenu-create-index-function 'semantic-create-imenu-index)
+		     (imenu-add-to-menubar "C++-Tags")
+		     ;; rebind as c-electric overrides global binding
+		     (define-key c-mode-base-map (kbd "C-c d") 'semantic-ia-show-doc)
+		     (define-key c-mode-base-map (kbd "C-c s") 'semantic-ia-show-summary)
+		     ;; want auto indentation more than parenthesis pairing
+		     ;; however, is there a way to have my cake and eat it too?
+		     (local-set-key "(" 'skeleton-pair-insert-maybe)
+		     (local-set-key "{" 'skeleton-pair-insert-maybe)
+		     (local-set-key "[" 'skeleton-pair-insert-maybe)
+		     (local-set-key (kbd "M-RET") 'newline-and-indent)
+		     ;; (c-toggle-auto-newline 1)
+		     ))
+  )
 
-;; force `c++-mode' for `*.h' header files
-(add-to-list 'auto-mode-alist (cons "\\.h\\'" 'c++-mode))
-;; add CUDA source & header files
-(add-to-list 'auto-mode-alist (cons "\\.cuh?\\'" 'c++-mode))
+(use-package c++-mode
+  :mode
+  (("\\.h\\'" . c++-mode)		;force `c++-mode' for `*.h' header files
+   ("\\.cuh?\\'" . c++-mode)		;CUDA source & header files
+   )
+  :config
+  ;; (setq root-include (let ((rootsys (getenv "ROOTSYS")))
+  ;; 		       (if rootsys (concat rootsys "/include")
+  ;; 			 "/usr/include/root")))
+  ;; (setq other-includes (list "/opt/data-an/include"))
+  ;; (dolist (dir (cons root-include other-includes))
+  ;;   (semantic-add-system-include dir 'c++-mode))
+  :hook
+  ((c++-mode . (lambda ()
+		 (lsp-deferred)
+		 ;; NOTE: *-language-standard: when set globally, breaks C mode
+		 (setq flycheck-clang-language-standard "c++17"
+		       ;; flycheck-clang-include-path (cons root-include other-includes)
+		       flycheck-gcc-language-standard "c++17"
+		       ;; flycheck-gcc-include-path (cons root-include other-includes)
+		       )))
+   )
+  )
 
-;; include directories
-(setq root-include (let ((rootsys (getenv "ROOTSYS")))
-		     (if rootsys (concat rootsys "/include")
-		       "/usr/include/root")))
-(setq other-includes (list "/opt/data-an/include"))
-
-(require 'dap-lldb)
-
-;; flycheck
-(add-hook 'c++-mode-hook #'lsp-deferred)
-(add-hook 'c++-mode-hook
-	  (lambda ()
-	    ;; NOTE: *-language-standard: when set globally, breaks C mode
-	    (setq flycheck-clang-language-standard "c++17"
-		  flycheck-clang-include-path (cons root-include other-includes)
-		  flycheck-gcc-language-standard "c++17"
-		  flycheck-gcc-include-path (cons root-include other-includes)
-		  )))
-
-;; semantic
-(dolist (dir (cons root-include other-includes))
-  (semantic-add-system-include dir 'c++-mode))
-
-(add-hook 'c-mode-common-hook
-	  (lambda ()
-	    (flycheck-mode 1)
-	    ;; for semantic imenu
-	    (setq imenu-create-index-function 'semantic-create-imenu-index)
-	    (imenu-add-to-menubar "C++-Tags")))
-
-(eval-after-load "cc-mode"
+(use-package cc-mode
+  :config
   (progn
     (load-library "semantic/ia")
     (load-library "semantic/senator")))
 
-
-;; TODO: reimplement `cedet-m3-menu-kbd' (cedet-m3.el) using popup.el
-
+(require 'dap-lldb)
 
 ;; FIXME: Doesn't quite do semantic isearch, could be bug in Emacs
 
@@ -66,36 +72,7 @@
   (let* ((senator-isearch-semantic-mode t))
     (senator-search-forward string bound noerror count)))
 
-
-;; ;; C/C++ language templates
-(add-hook 'c-mode-common-hook
-	  (lambda ()
-	    (unless (featurep 'yasnippet) (load-library "yasnippet"))))
-
-;; templates depend on mode
-(add-hook 'c-mode-hook
-	  (lambda ()
-	    (yas-minor-mode-on)))
-
-(add-hook 'c++-mode-hook
-	  (lambda ()
-	    (yas-minor-mode-on)))
-
-
-;;; Keybindings
-;; rebind as c-electric overrides global binding
-(add-hook 'c-mode-common-hook
-	  (lambda ()
-	    (define-key c-mode-base-map (kbd "C-c d") 'semantic-ia-show-doc)
-	    (define-key c-mode-base-map (kbd "C-c s") 'semantic-ia-show-summary)
-	    ;; want auto indentation more than parenthesis pairing
-	    ;; however, is there a way to have my cake and eat it too?
-	    (local-set-key "(" 'skeleton-pair-insert-maybe)
-	    (local-set-key "{" 'skeleton-pair-insert-maybe)
-	    (local-set-key "[" 'skeleton-pair-insert-maybe)
-	    (local-set-key (kbd "M-RET") 'newline-and-indent)
-	    ;; (c-toggle-auto-newline 1)
-	    ))
+;;; cpp-config.el ends here
 
 ;; Local Variables:
 ;; flycheck-disabled-checkers: (emacs-lisp emacs-lisp-checkdoc)
