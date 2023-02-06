324cb070f7b6330dfc4309d21ec98f7e9e64f52b
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2023-02-06 01:32:49 +0100

    Revert "lsp-config.el: move to use-package"
    This reverts commit 7e4bb8248906c877a56bce7283def67d5dbf9bf3.

--- lsp-config.el
+++ lsp-config.el
@@ -3,20 +3,16 @@
 ;;; Commentary:
 
 ;;; Code:
-(use-package lsp-mode
-  :ensure t
-  :bind (:map lsp-mode-map
-	      ("C-c ." . #'lsp-signature-activate)
-	      ("C-c d" . #'lsp-describe-thing-at-point)))
+(require 'lsp-mode)
 
-(use-package lsp-ui
-  :after lsp-mode
-  :config
-  :bind
-  (:map lsp-ui-mode-map
-	([f7] . #'lsp-ui-imenu)
-	([remap xref-find-definitions] . #'lsp-ui-peek-find-definitions)
-	([remap xref-find-references] . #'lsp-ui-peek-find-references)))
+(with-eval-after-load 'lsp-ui
+  (define-key lsp-ui-mode-map [f7] #'lsp-ui-imenu)
+  (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+  (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references))
+
+(with-eval-after-load 'lsp-mode
+  (define-key lsp-mode-map (kbd "C-c .") #'lsp-signature-activate)
+  (define-key lsp-mode-map (kbd "C-c d") #'lsp-describe-thing-at-point))
 
 ;;; lsp-config.el ends here
 
