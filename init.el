9bb20e06bef0b86e0eb3306d3806d60cbcb9c4de
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2024-12-26 02:58:57 +0100

    init.el: disable copilot

--- init.el
+++ init.el
@@ -73,7 +73,7 @@
 
 (load-file "~/.emacs.d/lsp-config.el")
 
-(load-file "~/.emacs.d/copilot-config.el")
+;; (load-file "~/.emacs.d/copilot-config.el")
 
 ;; Version control related customisations
 (load-file "~/.emacs.d/vc-config.el")
